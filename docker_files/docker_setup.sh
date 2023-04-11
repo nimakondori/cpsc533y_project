d1f522f382155e132b5842041ea5b20e4a34c62f
nimakondori <nimakondori96@gmail.com>
2023-04-11 15:33:26 -0700

    updated the bash files

---
+++ docker_files/docker_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# CUDA version from the command-line argument
+command=${1:-build}
+cuda_version=${2:-11.3}
+torch_version=${3:-1.12.1}
+final_tag=nimakondori/cpsc_533y_proj:torch_${torch_version}_cuda_${cuda_version}
+
+echo "Final Tag == $final_tag"
+
+if [[ "$command" == "build" ]]; then
+	docker build --build-arg cuda_version=$cuda_version --build-arg torch_version=$torch_version --tag $final_tag .
+
+	docker push $final_tag
+elif [[ "$command" == "run" ]]; then  
+	
+	 docker run -it -d \
+	          --gpus device=ALL \
+     	      --name=nima_cpsc533y_project  \
+      	      --volume=$HOME/workspace/repos/cpsc533y_project:/workspace/cpsc533y_project \
+      	      --volume=/home/armin/AorticStenosis.XAI_AS/data/as_tom:/mnt/data/ \
+			  --shm-size 4G\
+      	      $final_tag
+else
+	echo "invalid command. Use build or run"
+fi 
---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# CUDA version from the command-line argument
+
+cuda=${1:-"0"}
+filename=${2:-"run.py"}
+config_filename=${3:-"default.yml"}
+save_dir=${4:-"debug_run"}
+
+export CUDA_VISIBLE_DEVICES=$cuda
+
+command="python $filename --config_path configs/$config_filename --save_dir saved_files/$save_dir"
+echo "running the following command" $command
+
+# Run the command
+$command
+
